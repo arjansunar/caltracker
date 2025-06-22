80849bf7757c9eeadeca444a97afc475eb7bd49a
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-06-22 20:52:14 +0545

    fix: add type suffix

--- src/services/migrations.ts
+++ src/services/migrations.ts
@@ -1,17 +1,19 @@
 import { Data, Effect } from "effect";
 import { Pglite } from "./pglite";
-import { PGlite } from "@electric-sql/pglite";
+import type { PGlite } from "@electric-sql/pglite";
 
 import v0000 from "../drizzle/0000_left_power_pack.sql?raw";
 
 class MigrationsError extends Data.TaggedError("MigrationsError")<{
   cause: unknown;
 }> {}
+
 const execute = (client: PGlite) => (sql: string) =>
   Effect.tryPromise({
     try: () => client.exec(sql),
     catch: (error) => new MigrationsError({ cause: error }),
   });
+
 export class Migrations extends Effect.Service<Migrations>()("Migrations", {
   effect: Effect.gen(function* () {
     const db = yield* Pglite;
