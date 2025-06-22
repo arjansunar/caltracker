520daebc4f03777d2cc8b92b09c641ea5d8eb721
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-06-22 20:11:58 +0545

    feat: create pglite service

---
+++ src/services/pglite.ts
@@ -0,0 +1,22 @@
+import * as _PGlite from "@electric-sql/pglite";
+import { drizzle } from "drizzle-orm/pglite";
+import { Config, Data, Effect } from "effect";
+
+class PgliteError extends Data.TaggedError("PgliteError")<{ cause: unknown }> {}
+
+export class Pglite extends Effect.Service<Pglite>()("Pglite", {
+  effect: Effect.gen(function* () {
+    const indexDb = yield* Config.string("INDEX_DB");
+    const client = yield* Effect.tryPromise({
+      try: () => _PGlite.PGlite.create(`idb://${indexDb}`),
+      catch: (error) => new PgliteError({ cause: error }),
+    });
+    const orm = drizzle({ client });
+    const query = <R>(execute: (_: typeof orm) => Promise<R>) =>
+      Effect.tryPromise({
+        try: (_) => execute(orm),
+        catch: (error) => new PgliteError({ cause: error }),
+      });
+    return { client, orm, query };
+  }),
+}) {}
